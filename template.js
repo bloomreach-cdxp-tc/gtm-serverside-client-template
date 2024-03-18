bbfdbd5ee384d4fe663c05f11c9dc49feecdd6ea
a-change <a-change@users.noreply.github.com>
2024-03-18 23:19:03 +0100

    Update template.js
    - Combine Dominik's approach to validating paths with the initial idea to cache js sdk files
    - Add editor path to make experiments editor will work correctly
    - Add Access-Control-Request-Headers header processing

--- template.js
+++ template.js
@@ -1,199 +1,282 @@
-const claimRequest = require('claimRequest');
-const getCookieValues = require('getCookieValues');
-const getRequestBody = require('getRequestBody');
-const getRequestHeader = require('getRequestHeader');
-const getRequestMethod = require('getRequestMethod');
-const getRequestPath = require('getRequestPath');
-const getRequestQueryString = require('getRequestQueryString');
-const JSON = require('JSON');
-const logToConsole = require('logToConsole');
-const returnResponse = require('returnResponse');
-const setCookie = require('setCookie');
-const templateDataStorage = require('templateDataStorage');
-const sendHttpGet = require('sendHttpGet');
-const getTimestampMillis = require('getTimestampMillis');
-const sendHttpRequest = require('sendHttpRequest');
-const setResponseBody = require('setResponseBody');
-const setResponseHeader = require('setResponseHeader');
-const setResponseStatus = require('setResponseStatus');
-const getContainerVersion = require('getContainerVersion');
-const getRemoteAddress = require('getRemoteAddress');
+const claimRequest = require("claimRequest");
+const getCookieValues = require("getCookieValues");
+const getRequestBody = require("getRequestBody");
+const getRequestHeader = require("getRequestHeader");
+const getRequestMethod = require("getRequestMethod");
+const getRequestPath = require("getRequestPath");
+const getRequestQueryString = require("getRequestQueryString");
+const JSON = require("JSON");
+const logToConsole = require("logToConsole");
+const returnResponse = require("returnResponse");
+const setCookie = require("setCookie");
+const templateDataStorage = require("templateDataStorage");
+const sendHttpGet = require("sendHttpGet");
+const getTimestampMillis = require("getTimestampMillis");
+const sendHttpRequest = require("sendHttpRequest");
+const setResponseBody = require("setResponseBody");
+const setResponseHeader = require("setResponseHeader");
+const setResponseStatus = require("setResponseStatus");
+const getContainerVersion = require("getContainerVersion");
+const getRemoteAddress = require("getRemoteAddress");
 const path = getRequestPath();
+const getType = require('getType');
+
 
 // Check if this Client should serve exponea.js file
 if (path === data.proxyJsFilePath) {
-    claimRequest();
+	claimRequest();
 
-    const now = getTimestampMillis();
-    const thirty_minutes_ago = now - (30 * 60 * 1000);
+	const now = getTimestampMillis();
+	const thirty_minutes_ago = now - 30 * 60 * 1000;
 
-    if (templateDataStorage.getItemCopy('exponea_js') == null || templateDataStorage.getItemCopy('exponea_stored_at') < thirty_minutes_ago) {
-        sendHttpGet('https://api.exponea.com/js/exponea.min.js', {headers: {'X-Forwarded-For': getRemoteAddress()}}).then((result) => {
-            if (result.statusCode === 200) {
-                templateDataStorage.setItemCopy('exponea_js', result.body);
-                templateDataStorage.setItemCopy('exponea_headers', result.headers);
-                templateDataStorage.setItemCopy('exponea_stored_at', now);
-            }
-            sendProxyResponse(result.body, result.headers, result.statusCode);
-        });
-    } else {
-        sendProxyResponse(
-            templateDataStorage.getItemCopy('exponea_js'),
-            templateDataStorage.getItemCopy('exponea_headers'),
-            200
-        );
-    }
+	// save js sdk to server cache if it's not been done yet
+	if (templateDataStorage.getItemCopy("exponea_js") == null || templateDataStorage.getItemCopy("exponea_stored_at") < thirty_minutes_ago) {
+		sendHttpGet(data.targetAPI + "/js/exponea.min.js", { headers: { "X-Forwarded-For": getRemoteAddress() } }).then((result) => {
+			if (result.statusCode === 200) {
+				templateDataStorage.setItemCopy("exponea_js", result.body);
+				templateDataStorage.setItemCopy("exponea_headers", result.headers);
+				templateDataStorage.setItemCopy("exponea_stored_at", now);
+			}
+			sendProxyResponse(result.body, result.headers, result.statusCode);
+		});
+	} else {
+		sendProxyResponse(templateDataStorage.getItemCopy("exponea_js"), templateDataStorage.getItemCopy("exponea_headers"), 200);
+	}
 }
 
 // Check if this Client should serve exponea.js.map file (Just only to avoid annoying error in console)
-if (path === '/exponea.min.js.map') {
-    sendProxyResponse('{"version": 1, "mappings": "", "sources": [], "names": [], "file": ""}', {'Content-Type': 'application/json'}, 200);
+if (path === "/exponea.min.js.map") {
+	sendProxyResponse('{"version": 1, "mappings": "", "sources": [], "names": [], "file": ""}', { "Content-Type": "application/json" }, 200);
 }
+
+const cookieWhiteList = ["xnpe_" + data.projectToken, "__exponea_etc__", "__exponea_time2__"];
+const headerWhiteList = ["referer", "user-agent", "etag", "Access-Control-Request-Headers"];
+
+const validPaths = [
+	"/bulk",
+	"/managed-tags/show",
+	"/campaigns/banners/show",
+	"/campaigns/experiments/show",
+	"/campaigns/html/get",
+	"/optimization/recommend/user",
+	"/webxp/projects/",
+	"/webxp/data/modifications/",
+	"/webxp/bandits/reward",
+	"/webxp/script-async/",
+	"/webxp/script/",
+    "/editor",
+];
+
+let isValidPath = false;
 
 // Check if this Client should claim request
-if (path !== '/bulk' && path !== '/managed-tags/show' && path !== '/campaigns/banners/show' && path !== ('/webxp/projects/'+data.projectToken+'/bundle')) {
-    return;
+if (validPaths.reduce((res, validPath) => res || equalOrStartsOrEnds(path, validPath, true), false)) {
+	log({
+		Name: "Exponea",
+		Type: "Valid path",
+		path: path,
+	});
+	isValidPath = true;
 }
 
-claimRequest();
+if (!isValidPath) {
+	return;
+} else {
+	claimRequest();
+
+	const traceId = getRequestHeader("trace-id");
+	const requestOrigin = getRequestHeader("Origin");
+	const requestMethod = getRequestMethod();
+	const requestBody = getRequestBody();
+	const requestUrl = generateRequestUrl();
+	const requestHeaders = generateRequestHeaders();
+
+	log({
+		Name: "Exponea",
+		Type: "Request",
+		TraceId: traceId,
+		RequestOrigin: requestOrigin,
+		RequestMethod: requestMethod,
+		RequestUrl: requestUrl,
+		RequestHeaders: requestHeaders,
+		RequestBody: requestBody,
+	});
+	sendHttpRequest(requestUrl, { method: requestMethod, headers: requestHeaders }, requestBody).then((result) => {
+		log({
+			Name: "Exponea",
+			Type: "Response",
+			TraceId: traceId,
+			ResponseStatusCode: result.statusCode,
+			ResponseHeaders: result.headers,
+			ResponseBody: result.body,
+		});
+
+		for (const key in result.headers) {
+			if (key === "set-cookie") {
+				setResponseCookies(result.headers[key]);
+			} else {
+				setResponseHeader(key, result.headers[key]);
+			}
+		}
 
 
-const cookieWhiteList = ['xnpe_' + data.projectToken, '__exponea_etc__', '__exponea_time2__'];
-const headerWhiteList = ['referer', 'user-agent', 'etag'];
+        // Access-Control-Request-Headers header is now sent with the modifications data request, which will fail unless Access-Control-Request-Headers is set
+        if (requestHeaders["Access-Control-Request-Headers"]) {
+			setResponseHeader("Access-Control-Allow-Headers", requestHeaders["Access-Control-Request-Headers"]);
+		}
 
-const containerVersion = getContainerVersion();
-const isDebug = containerVersion.debugMode;
-const isLoggingEnabled = determinateIsLoggingEnabled();
-const traceId = getRequestHeader('trace-id');
+		setResponseBody(result.body || "");
+		setResponseStatus(result.statusCode);
 
-const requestOrigin = getRequestHeader('Origin');
-const requestMethod = getRequestMethod();
-const requestBody = getRequestBody();
-const requestUrl = generateRequestUrl();
-const requestHeaders = generateRequestHeaders();
+		if (requestOrigin) {
+			setResponseHeader("access-control-allow-origin", requestOrigin);
+			setResponseHeader("access-control-allow-credentials", "true");
+		}
 
-if (isLoggingEnabled) {
-    logToConsole(JSON.stringify({
-        'Name': 'Exponea',
-        'Type': 'Request',
-        'TraceId': traceId,
-        'RequestOrigin': requestOrigin,
-        'RequestMethod': requestMethod,
-        'RequestUrl': requestUrl,
-        'RequestHeaders': requestHeaders,
-        'RequestBody': requestBody,
-    }));
+		returnResponse();
+	});
 }
-
-sendHttpRequest(requestUrl, {method: requestMethod, headers: requestHeaders}, requestBody).then((result) => {
-    if (isLoggingEnabled) {
-        logToConsole(JSON.stringify({
-            'Name': 'Exponea',
-            'Type': 'Response',
-            'TraceId': traceId,
-            'ResponseStatusCode': result.statusCode,
-            'ResponseHeaders': result.headers,
-            'ResponseBody': result.body,
-        }));
-    }
-
-    for (const key in result.headers) {
-        if (key === 'set-cookie') {
-            setResponseCookies(result.headers[key]);
-        } else {
-            setResponseHeader(key, result.headers[key]);
-        }
-    }
-
-    setResponseBody(result.body);
-    setResponseStatus(result.statusCode);
-
-    if (requestOrigin) {
-        setResponseHeader('access-control-allow-origin', requestOrigin);
-        setResponseHeader('access-control-allow-credentials', 'true');
-    }
-
-    returnResponse();
-});
-
 
 function generateRequestUrl() {
-    let url = data.targetAPI + getRequestPath();
-    const queryParams = getRequestQueryString();
+	let url = combineURLs(data.targetAPI, getRequestPath());
+	const queryParams = getRequestQueryString();
 
-    if (queryParams) url = url + '?' + queryParams;
+	if (queryParams) url = url + "?" + queryParams;
 
-    return url;
+	return url;
 }
 
 function generateRequestHeaders() {
-    let headers = {};
-    let cookies = [];
+	let headers = {};
+	let cookies = [];
 
-    for (let i = 0; i < headerWhiteList.length; i++) {
-        let headerName = headerWhiteList[i];
-        let headerValue = getRequestHeader(headerName);
+	for (let i = 0; i < headerWhiteList.length; i++) {
+		let headerName = headerWhiteList[i];
+		let headerValue = getRequestHeader(headerName);
 
-        if (headerValue) {
-            headers[headerName] = getRequestHeader(headerName);
-        }
-    }
+		if (headerValue) {
+			headers[headerName] = getRequestHeader(headerName);
+		}
+	}
 
-    headers.cookie = '';
+	headers.cookie = "";
 
-    for (let i = 0; i < cookieWhiteList.length; i++) {
-        let cookieName = cookieWhiteList[i];
-        let cookieValue = getCookieValues(cookieName);
+	for (let i = 0; i < cookieWhiteList.length; i++) {
+		let cookieName = cookieWhiteList[i];
+		let cookieValue = getCookieValues(cookieName);
 
-        if (cookieValue && cookieValue.length) {
-            cookies.push(cookieName + '=' + cookieValue[0]);
-        }
-    }
+		if (cookieValue && cookieValue.length) {
+			cookies.push(cookieName + "=" + cookieValue[0]);
+		}
+	}
 
-    headers.cookie = cookies.join('; ');
-    headers['X-Forwarded-For'] = getRemoteAddress();
+	headers.cookie = cookies.join("; ");
+	headers["X-Forwarded-For"] = getRemoteAddress();
 
-    return headers;
+	return headers;
 }
 
 function setResponseCookies(setCookieHeader) {
-    for (let i = 0; i < setCookieHeader.length; i++) {
-        let setCookieArray = setCookieHeader[i].split('; ').map(pair => pair.split('='));
-        let setCookieJson = '';
+	for (let i = 0; i < setCookieHeader.length; i++) {
+		let setCookieArray = setCookieHeader[i].split("; ").map((pair) => pair.split("="));
+		let setCookieJson = "";
 
-        for (let j = 1; j < setCookieArray.length; j++) {
-            if (j === 1) setCookieJson += '{';
-            if (setCookieArray[j].length > 1) setCookieJson += '"' + setCookieArray[j][0] + '": "' + setCookieArray[j][1] + '"'; else setCookieJson += '"' + setCookieArray[j][0] + '": ' + true;
-            if (j + 1 < setCookieArray.length) setCookieJson += ','; else setCookieJson += '}';
-        }
+		for (let j = 1; j < setCookieArray.length; j++) {
+			if (j === 1) setCookieJson += "{";
+			if (setCookieArray[j].length > 1) setCookieJson += '"' + setCookieArray[j][0] + '": "' + setCookieArray[j][1] + '"';
+			else setCookieJson += '"' + setCookieArray[j][0] + '": ' + true;
+			if (j + 1 < setCookieArray.length) setCookieJson += ",";
+			else setCookieJson += "}";
+		}
 
-        setCookie(setCookieArray[0][0], setCookieArray[0][1], JSON.parse(setCookieJson));
-    }
+		setCookie(setCookieArray[0][0], setCookieArray[0][1], JSON.parse(setCookieJson));
+	}
 }
 
 function sendProxyResponse(response, headers, statusCode) {
-    setResponseStatus(statusCode);
-    setResponseBody(response);
+	setResponseStatus(statusCode);
+	setResponseBody(response);
 
-    for (const key in headers) {
-        setResponseHeader(key, headers[key]);
-    }
+	for (const key in headers) {
+		setResponseHeader(key, headers[key]);
+	}
 
-    returnResponse();
+	returnResponse();
 }
 
 function determinateIsLoggingEnabled() {
-    if (!data.logType) {
-        return isDebug;
-    }
+	const containerVersion = getContainerVersion();
+	const isDebug = containerVersion.debugMode;
+	if (!data.logType) {
+		return isDebug;
+	}
 
-    if (data.logType === 'no') {
-        return false;
-    }
+	if (data.logType === "no") {
+		return false;
+	}
 
-    if (data.logType === 'debug') {
-        return isDebug;
-    }
+	if (data.logType === "debug") {
+		return isDebug;
+	}
 
-    return data.logType === 'always';
+	return data.logType === "always";
+}
+
+function log(data) {
+	const isLoggingEnabled = determinateIsLoggingEnabled();
+	if (isLoggingEnabled) {
+		logToConsole(JSON.stringify(data));
+	}
+}
+
+function startsWith(string, substring) {
+	return string.indexOf(substring) === 0;
+}
+
+function endsWith(string, substring) {
+	const lastIndex = string.lastIndexOf(substring);
+	return lastIndex > -1 && string.length === lastIndex + substring.length;
+}
+
+function equalOrStartsOrEnds(string, substring, withWildcard) {
+	if (substring.length === 0) return false;
+
+	const containWildcard = substring.indexOf("*") > -1;
+	const eq = string === substring;
+	if (eq) return true;
+	log({
+		Name: "Exponea",
+		Type: "EqualOrStartsOrEnds",
+		string: string,
+		substring: substring,
+		withWildcard: withWildcard,
+		containWildcard: containWildcard,
+	});
+	if (!withWildcard || !containWildcard) {
+		return startsWith(string, substring) || endsWith(string, substring);
+	} else {
+		const parsedSubstring = withWildcard ? substring.split("*", 2) : [substring];
+		return startsWith(string, parsedSubstring[0]) && endsWith(string, parsedSubstring[1]);
+	}
+}
+
+function splitAndSanitize(arr) {
+	if (getType(arr) !== "string") return [];
+	return arr
+		.split(",")
+		.map((a) => a.trim())
+		.filter((a) => a);
+}
+
+function removeEndSlash(string) {
+	if (endsWith(string, "/")) return string.slice(0, -1);
+	return string;
+}
+function removeStartSlash(string) {
+	if (startsWith(string, "/")) return string.slice(1);
+	return string;
+}
+
+function combineURLs(baseURL, relativeURL) {
+	return relativeURL ? removeEndSlash(baseURL) + "/" + removeStartSlash(relativeURL) : baseURL;
 }
